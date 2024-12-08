be2331c3f4cbea003ad0dbefc157717a5ebb3b2d
aoiro.hncd <aoiro.hn@gmail.com>
2024-12-08 21:59:11 +0900

    drawerを修正

--- packages/imagen/imagen/getImage.py
+++ packages/imagen/imagen/getImage.py
@@ -33,24 +33,5 @@ def get_url_image(url: str, dir: PathLike, name: str):
 
         page = context.new_page()
         page.goto(url)
-        page.screenshot(path=str(Path(dir) / name), full_page=True, timeout=50000)
+        page.screenshot(path=str(Path(dir) / name), full_page=True)  # timeout=50000
         browser.close()
-
-
-# def get_image(url: str, dir: PathLike, name: str):
-#     hti = Html2Image(
-#         size=(800 * 2, 480 * 2),
-#         output_path=str(Path(dir)),
-#         custom_flags=CHROMIUM_FLAGS,
-#     )
-
-#     # hti.screenshot(url="http://localhost:5173/dashboard", save_as=str(Path(name)))
-#     hti.screenshot(
-#         url=url,
-#         save_as=str(Path(name)),
-#         # css_str=[
-#         #     "body {background-color: red;}",
-#         #     "body {transform: matrix(2, 0, 0, 2, 0, 0)}",
-#         # ],
-#         css_str=["body {background: red;}", "body {font-size: 50px;}"],
-#     )
