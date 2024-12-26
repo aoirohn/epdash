ec76bf28a9a73ef914f55c5139fd9d31f2a57ea2
aoiro.hncd <aoiro.hn@gmail.com>
2024-12-26 11:35:20 +0900

    clientではdrawのみを行う

--- packages/drawer/draw-dashboard.py
+++ packages/drawer/draw-dashboard.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 import os
-from drawer.draw import draw
-import imagen
+from drawer import draw, get_url_image
 from dotenv import load_dotenv
 
 load_dotenv()
 
 url = os.getenv("DASHBOARD_URL")
 
-img = imagen.get_dithered_image(url)
+if url is None:
+    print("Please set DASHBOARD_URL in .env file")
+    exit()
+
+print("get image from :", url)
+
+img = get_url_image(url)
 
 print("draw image")
 draw("epd7in3f", img)
